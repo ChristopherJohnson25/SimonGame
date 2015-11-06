977adb97fd873118d2ac9201ffdc1dac7e5896fd
Chris Johnson <johnsonchris98@gmail.com>
2015-11-06 10:57:37 -0500

    day 2

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-$(document).ready(function(){})
+// $(document).ready(function(){})
+
+console.log("linked");
+
+var alien = document.querySelector(".topAlien");
+var button = document.querySelector(".bottomButton");
+var start = document.getElementById('starter');
+
+var clicked = function(){
+	console.log("clicked");
+};
+
+button.addEventListener('click', clicked);
+
